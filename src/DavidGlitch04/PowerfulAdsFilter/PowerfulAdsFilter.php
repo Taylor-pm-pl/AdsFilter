74a318010c252ec0203c249da35699b9b93eafbf
YTBJero <nguyenhieucoder47@gmail.com>
2022-03-20 16:46:08 +0700

    Added function isIP()

--- src/DavidGlitch04/PowerfulAdsFilter/PowerfulAdsFilter.php
+++ src/DavidGlitch04/PowerfulAdsFilter/PowerfulAdsFilter.php
@@ -10,4 +10,16 @@ class PowerfulAdsFilter extends PluginBase{
     protected function onEnable(): void{
         $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
     }
+
+    /**
+     * @param string $ip
+     * @return bool
+     */
+    public function isIP(string $ip): bool{
+        if(filter_var($ip, FILTER_VALIDATE_IP)){
+            return true;
+        } else{
+            return false;
+        }
+    }
 }
