9cf9a6915efb9da7342f75e8ce92d018a38d75e5
YTBJero <nguyenhieucoder47@gmail.com>
2022-03-20 16:54:37 +0700

    Use PlayerChatEvent

--- src/DavidGlitch04/PowerfulAdsFilter/PowerfulAdsFilter.php
+++ src/DavidGlitch04/PowerfulAdsFilter/PowerfulAdsFilter.php
@@ -10,16 +10,4 @@ class PowerfulAdsFilter extends PluginBase{
     protected function onEnable(): void{
         $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
     }
-
-    /**
-     * @param string $ip
-     * @return bool
-     */
-    public function isIP($ip): bool{
-        if(filter_var($ip, FILTER_VALIDATE_IP)){
-            return true;
-        } else{
-            return false;
-        }
-    }
 }
